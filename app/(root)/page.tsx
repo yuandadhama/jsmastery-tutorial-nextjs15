af8950f7d5db5fdf8ae1d9f92db9d481d386db38
yuandadhama <yuandadhama@gmail.com>
2025-01-23 20:08:58 +0700

    Search box is done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({ searchParams } : {searchParams: Promise<{query? : string}>}) {
+  const { query } = await searchParams;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+        <SearchForm query={query}/>
+      </section>
+    </>
+  );
+}
